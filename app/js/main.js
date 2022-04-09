a99f2a12ac5c9cab45327e3cb7b329bf0de0784a
elmar98 <a-xomchenko@bk.ru>
2022-04-09 10:57:43 +0600

    added header, main, slider, info, product, footer, added fonts

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,26 @@
 $(function(){
+	
+	$('.slider').slick({
+		nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+		prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+	})
+
+	$('.catalog__menu').on('click', function() {
+		$(this).toggleClass('catalog__menu--active');
+		$('.catalog__list').toggleClass('catalog__list--active');
+	});
+
+	$('.catalog__list').on('click', function() {
+		$('.catalog__menu').removeClass('catalog__menu--active');
+		$('.catalog__list').removeClass('catalog__list--active');
+	});
+
+	$('html').on('click', function(event){
+		if (!$(event.target).closest('.catalog__menu').length) {
+		  $('.catalog__menu').removeClass('catalog__menu--active');
+		  $('.catalog__list').removeClass('catalog__list--active');
+		}
+	 });
+
+	 var mixer = mixitup('.product__cards');
 });
--- gulpfile.js
+++ gulpfile.js
@@ -61,6 +61,9 @@ function cleanDist(){
 function scripts(){
 	return src([
 		'node_modules/jquery/dist/jquery.js',
+		'node_modules/slick-carousel/slick/slick.js',
+		'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+		'node_modules/mixitup/dist/mixitup.js',
 		'app/js/main.js'
 	])
 	.pipe(concat('main.min.js'))
